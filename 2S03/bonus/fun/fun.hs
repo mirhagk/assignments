b2a4ab0cd7e71dbc050653d34d4dbd1519b31204
Nathan Jervis <mirhagk@gmail.com>
2013-11-16 16:16:46 -0500

    Added Pretty Print and simplify functions

--- 2S03/bonus/fun/fun.hs
+++ 2S03/bonus/fun/fun.hs
@@ -6,26 +6,26 @@
 --r2c1 r2c2 r2c3 
 --r3c1 r3c2 r3c3
 
-data Code = Const Int
-		| Mult Code Code
-		| Add Code Code
+data Expr = Const Int
+		| Mult Expr Expr
+		| Add Expr Expr
 		| Var [Char]
 		deriving (Show)
 
 codeDet n = codeDetIn 1 0 1 n True
 
 
-makeField:: Int -> Int -> Code
+makeField:: Int -> Int -> Expr
 makeField r c = Var ("r"++ show r ++ "c"++ show c)
 codeDetIn ro co _ 1 _ = makeField ro co
 codeDetIn ro co i n s = if (i>n) then (Const 0) else Add inner (codeDetIn ro co (i+1) n (not s))
 		where field = makeField ro (co+i)
 		      inner = Mult field (codeDetIn (ro+1) (co+1) (1) (n-1) True)
 			
-codeSum :: [Code] -> Code			
+codeSum :: [Expr] -> Expr			
 codeSum (x:[]) = x
 codeSum (x:xs) = Add x (codeSum xs)
-codeDetMat :: [[[Char]]] -> Code
+codeDetMat :: [[[Char]]] -> Expr
 codeDetMat m = if (length m == 1) then Var (m!!0!!0) else codeSum [ neg (term x) x | x <-[0..(length m)-1]]
 	where neg x i = if mod i 2 == 0 then x else Mult x (Const (-1))
 	      term x = Mult (Var (m!!0!!x)) (codeDetMat (subMatrix m 0 x))
@@ -38,6 +38,38 @@ astToEquation (Add x y) = astToEquation_doOp x y "+"
 astToEquation (Mult x y) = astToEquation_doOp x y "*"
 
 astToEquation_doOp x y op = "(" ++ astToEquation x ++ ") " ++ op ++ " (" ++ astToEquation y ++ ")"
+
+
+prettyPrint p = case (simplify p) of
+				(Mult (Const c) (Var x)) -> show c ++ x
+				(Const c) -> show c
+				(Var x) -> x
+				(Add x y) -> "(" ++ (prettyPrint x) ++ ") + (" ++ (prettyPrint y) ++ ")"
+				(Mult x y) -> "(" ++ (prettyPrint x) ++ ") * (" ++ (prettyPrint y) ++ ")"
+
+
+simplify :: Expr -> Expr
+simplify (Add p q) = case (simplify p, simplify q) of
+                          (Const 0, x) -> x
+                          (x, Const 0) -> x
+                          (Const pc, Const qc) -> Const (pc + qc)
+                          (Var pv, Var qv) -> if pv == qv then simplify (Mult (Var pv) (Const 2)) else Add (Var pv) (Var qv)
+                          (Const pc, Add (Const qc) notConst) -> Add (Const (pc + qc)) notConst
+                          (pNonConst, Add (Const qc) qNonConst) -> Add (Const qc) (Add pNonConst qNonConst) -- New line
+                          (nonConst, Const qc) -> Add (Const qc) nonConst
+                          (pNonConst, qNonConst) -> Add pNonConst qNonConst
+simplify (Mult p q) = case (simplify p, simplify q) of
+                          (Const 0, _) -> Const 0
+                          (_, Const 0) -> Const 0
+                          (Const 1, x) -> x
+                          (x, Const 1) -> x
+                          (Const pc, Const qc) -> Const (pc * qc)
+                          (Const pc, Mult (Const qc) notConst) -> Mult (Const (pc * qc)) notConst
+                          (pNonConst, Mult (Const qc) qNonConst) -> Mult (Const qc) (Mult pNonConst qNonConst) -- New line
+                          (nonConst, Const qc) -> Mult (Const qc) nonConst
+                          (pNonConst, qNonConst) -> Mult pNonConst qNonConst
+simplify p = p
+
 		
 testMatrices = [
 				[					
